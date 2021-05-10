3f98c6d6a5f3681e076aa1d981169a4ac4580f69
Ilia Shafirov <shafirov1999@gmail.com>
2021-05-10 22:56:58 +0300

    Initial Commit

---
+++ Reminders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminders
+//
+//  Created by Elena Shafirova on 10.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
